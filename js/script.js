fcea8b27ab553ee681eedeae89b9f22522482e2e
Omar Abdelhamid <omardemiro@gmail.com>
2025-08-12 23:34:24 +0200

    Re-make

--- js/script.js
+++ js/script.js
@@ -1,205 +1,52 @@
-var lnStickyNavigation;
+// Mobile menu toggle
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+const navLinks = document.querySelector('.nav-links');
 
-$(document).ready(function()
-{	
-	applyHeader();
-	applyNavigation(); 
-	applyMailTo();
-	applyResize();
-	checkHash();
-	checkBrowser();
+mobileMenuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    mobileMenuBtn.innerHTML = navLinks.classList.contains('active') ? 
+        '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
 });
 
-/* HEADER FUNCTIONS */
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 80,
+                behavior: 'smooth'
+            });
+            
+            // Close mobile menu if open
+            if (navLinks.classList.contains('active')) {
+                navLinks.classList.remove('active');
+                mobileMenuBtn.innerHTML = '<i class="fas fa-bars"></i>';
+            }
+        }
+    });
+});
 
-function applyHeader()
-{
-	$('.jumbotron').css({ height: ($(window).height()) +'px' });
-	$('.container').css({ 'min-height': ($(window).height()) +'px' });
-	
-	lazyLoad($('.jumbotron'));
-}	
+// Header scroll effect
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.1)';
+        header.style.backgroundColor = 'white';
+    } else {
+        header.style.boxShadow = 'none';
+        header.style.backgroundColor = 'white';
+    }
+});
 
-function lazyLoad(poContainer)
-{
-	/*var lstrSource   = poContainer.attr('data-src');
-	var lstrPosition = poContainer.attr('data-position');
-
-	$('<img>').attr('src', lstrSource).load(function()
-	{
-		poContainer.css('background-image', 'url("'+ lstrSource +'")');
-		poContainer.css('background-position', lstrPosition);
-		poContainer.css('-ms-filter', '"progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\')"');
-		poContainer.css('filter', 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\'' + lstrSource + '\', sizingMethod=\'scale\'');
-	});*/
-}
-
-/* NAVIGATION FUNCTIONS */
-
-function applyNavigation()
-{
-	applyClickEvent();
-	applyNavigationFixForPhone();
-	applyScrollSpy();
-	applyStickyNavigation();
-}
-
-function applyClickEvent()
-{
-	$('a[href*=#]').on('click', function(e)
-	{
-		e.preventDefault();
-		
-		if( $( $.attr(this, 'href') ).length > 0 )
-		{
-			$('html, body').animate(
-			{
-				scrollTop: $( $.attr(this, 'href') ).offset().top
-			}, 400);
-		}
-		return false;
-	});
-}
-
-function applyNavigationFixForPhone()
-{
-	$('.navbar li a').click(function(event) 
-	{
-		$('.navbar-collapse').removeClass('in').addClass('collapse');
-	});
-}
-
-function applyScrollSpy()
-{
-	$('#navbar-example').on('activate.bs.scrollspy', function() 
-	{
-		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
-	});
-}
-
-function applyStickyNavigation()
-{
-	lnStickyNavigation = $('.scroll-down').offset().top + 20;
-	
-	$(window).on('scroll', function() 
-	{  
-		stickyNavigation();  
-	});  
-	
-	stickyNavigation();
-}
-
-function stickyNavigation()
-{         
-	if($(window).scrollTop() > lnStickyNavigation) 
-	{   
-		$('body').addClass('fixed');  
-	} 
-	else 
-	{  
-		$('body').removeClass('fixed');   
-	}  
-}
-
-/* MAILTO FUNCTION */
-
-function applyMailTo()
-{
-	$('a[href*=mailto]').on('click', function(e)
-	{
-		var lstrEmail = $(this).attr('href').replace('mailto:', '');
-		
-		lstrEmail = lstrEmail.split('').reverse().join('')
-		
-		$(this).attr('href', 'mailto:' + lstrEmail);
-	});
-}
-
-/* RESIZE FUNCTION */
-
-function applyResize()
-{
-	$(window).on('resize', function() 
-	{  
-		lnStickyNavigation = $('.scroll-down').offset().top + 20;
-	
-		$('.jumbotron').css({ height: ($(window).height()) +'px' });
-	}); 
-}
-
-/* HASH FUNCTION */
-
-function checkHash()
-{
-	lstrHash = window.location.hash.replace('#/', '#');
-	
-	if($('a[href='+ lstrHash +']').length > 0)
-	{
-		$('a[href='+ lstrHash +']').trigger('click');
-	}
-}
-
-/* IE7- FALLBACK FUNCTIONS */
-
-function checkBrowser()
-{
-	var loBrowserVersion = getBrowserAndVersion();
-	
-	if(loBrowserVersion.browser == 'Explorer' && loBrowserVersion.version < 8)
-	{ 
-		$('#upgrade-dialog').modal({
-			backdrop: 'static',
-			keyboard: false
-		});
-	}
-}
-
-function getBrowserAndVersion() 
-{
-	var laBrowserData = [{
-		string: 		navigator.userAgent,
-		subString: 		'MSIE',
-		identity: 		'Explorer',
-		versionSearch: 	'MSIE'
-	}];
-	
-	return {
-		browser: searchString(laBrowserData) || 'Modern Browser',
-		version: searchVersion(navigator.userAgent) || searchVersion(navigator.appVersion) || '0.0'
-	};
-}
-
-function searchString(paData) 
-{
-	for(var i = 0; i < paData.length; i++)	
-	{
-		var lstrDataString 	= paData[i].string;
-		var lstrDataProp 	= paData[i].prop;
-		
-		this.versionSearchString = paData[i].versionSearch || paData[i].identity;
-		
-		if(lstrDataString) 
-		{
-			if(lstrDataString.indexOf(paData[i].subString) != -1)
-			{
-				return paData[i].identity;
-			}
-		}
-		else if(lstrDataProp)
-		{
-			return paData[i].identity;
-		}
-	}
-}
-	
-function searchVersion(pstrDataString) 
-{
-	var lnIndex = pstrDataString.indexOf(this.versionSearchString);
-	
-	if(lnIndex == -1) 
-	{
-		return;
-	}
-	
-	return parseFloat(pstrDataString.substring(lnIndex + this.versionSearchString.length + 1));
-}	
+// Set current year in footer
+document.addEventListener('DOMContentLoaded', function() {
+    const yearSpan = document.getElementById('current-year');
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+});
